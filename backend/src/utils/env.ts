d5975d5eb1c7bfcdfe8604a0a3767d3f36f0fe05
VictorHneg <142644242+VictorHneg@users.noreply.github.com>
2025-06-10 09:08:45 -0300

    Delete backend/src directory

--- backend/src/utils/env.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const envSchema = z.object({
-  FRONTEND_URL: z.string().url(),
-  PORT: z.string().optional(),
-  // Adicione outras variáveis de ambiente conforme necessário
-});
